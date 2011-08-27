188928dbad334111633d611d5e2dde4c5484d769
Angeline Gragasin <angelinegragasin@angeline-gragasins-imac.local>
2011-08-26 19:54:54 -0700

    test factory

--- public/js/game.js
+++ public/js/game.js
@@ -1,4 +1,30 @@
 var winx, winy, mouse_coordinates = [];
+winx = window.innerWidth;
+	winy = window.innerHeight;
+
+
+//RAPHAEL INIT
+
+paper = Raphael('viewPort', 5000, winy);
+g = winy/10;
+function graph(){
+	alphaGraph = {}
+	var iable = winy/25, xine = 5000/iable;
+	for (i=0;i<xine;++i){
+		paper.path("M"+iable*i+" 0L"+iable*i+" "+winy).attr({"stroke":"rgba(98,202,263,.2)"});
+	}
+	for (i=0;i<$('#console').width()/(winy/100);++i){
+		paper.path("M"+(winy/100)*i+" 0L"+(winy/100)*i+" "+winy).attr({"stroke":"rgba(98,202,263,.1)"});
+	}
+	for (i=0;i<25;++i){
+		paper.path("M0 "+iable*i+"L"+5000+" "+iable*i).attr({"stroke":"rgba(98,202,263,.2)"});
+	}
+	for (i=0;i<100;++i){
+		paper.path("M0 "+(winy/100)*i+"L"+5000+" "+(winy/100)*i).attr({"stroke":"rgba(98,202,263,.1)"});
+	}
+}graph()
+
+// socket.io init
 
 var socket = io.connect();
 
@@ -6,21 +32,21 @@ var socket = io.connect();
    console.log('connection made')
  });
 
-		socket.on("moveXY", function (x, y){
-			//alphaCircle[3].anim8(x,y);
-			alphaCircle[1].motion(x, y)
-//			console.log(x+','+y)
-		});
+socket.on('event', function(a,b,c){
+	console.log('event!')
+	$('#msg').empty().append("event type: "+a+"<br>Command: "+b+"<br>At Coordinate: "+c[0]+","+c[1]);
+})
+
 
 var game = {
 
-	init = function(){
+	init : function(){
 		winx = window.innerWidth;
-    winy = window.innerHeight;
+    	winy = window.innerHeight;
 		$('#viewPort').mousemove(function(e){
-			mouse_coordinate = [e.pageX, e.pageY]
+			mouse_coordinates = [e.pageX, e.pageY]
 		});
-		
+		this.event_emitter(); 
 	},
 
 	cmds:{
@@ -32,18 +58,19 @@ var game = {
 		var z = this
 		, kup = function(e){
 			var code = e.which||e.keyCode;
-			socket.emit("key", code, mouse_coordinates)
+			socket.emit("event", "key", code, mouse_coordinates)
 		}
 		, click_fn = function(e){
-			socket.emit("click", z.cmds.click, mouse_coordinates)
+			socket.emit("event", "click", z.cmds.click, mouse_coordinates)
 		}
 		, dbl_fn = function(e){
-			socket.emit("dblClick", z.cmds.dbl, mouse_coordinates)
+			socket.emit("event", "dblClick", z.cmds.dbl, mouse_coordinates)
 		};
 		$('body').bind('keyup',kup);
 		$('#viewPort').bind('click',click_fn);
 		$('#viewPort').bind('dblclick',dbl_fn)
 	}
-	
-	
 }
+$(function(){
+	game.init();
+});
