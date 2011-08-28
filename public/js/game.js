5f9ecc94f32247522872c011f45d6591b7b1a360
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-08-28 09:16:01 -0700

    tick tick

--- public/js/game.js
+++ public/js/game.js
@@ -53,7 +53,7 @@ _game = game = {
 	},
 	my_energy: 66, // it is 5:17 am after all
 	fluxCapacity:{
-		get:0, 
+		get:66, 
 		set : function(x){
 			var y = x;
 			if (x > 100){
@@ -114,7 +114,7 @@ _game = game = {
 			var xcoord = (e.clientX - parseInt(vp.css("left")));
 			var ycoord = (e.clientY - parseInt(vp.css("top")));
 			parseInt(vp.css("left")) + (e.clientX - winx/2)
-			z.socket.emit("event", "click", z.cmds.click, [xcoord,ycoord]);
+			z.socket.emit("event", "click", z.cmds.click, [xcoord,ycoord], this.fluxCapacity.get);
 			$('#viewPort').css({left: parseInt(vp.css("left")) - (e.clientX - winx/2), top: parseInt(vp.css("top")) - (e.clientY - winy/2)})
 		}
 		, getDelta = function(e){
@@ -408,7 +408,7 @@ _game.draw = {
 		};
 		this.paper = Raphael('viewPort', width, height);
 		this.commander = Raphael('console', this.winx, this.winy);
-		
+		game.fluxCapacity.set(66)
 	},
 	graph:function(){
 		
