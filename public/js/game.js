ed6cb1841fdfdfab8c52ff72ed810ad8ebe50c59
Ryan Day <soldair@gmail.com>
2011-09-02 10:12:34 -0700

    wip debug for moveToward function

--- public/js/game.js
+++ public/js/game.js
@@ -386,19 +386,28 @@ _game = game = {
 _game.math = {
  	moveToward:function(c1,c2,distance,constrain) {
 
-		var slope = this.slope(c1,c2)
-		c3 = [c1[0],c1[1]];
+		//console.log('start ',c1,' dest ',c2);
 		
 
-		var x = distance-slope
-		,y = slope*x;
+		var c3 = [c1[0],c1[1]]
+		, x = 0
+		, y = 0;
+		
+		if(c1[0] == c2[0]) {
+			y = distance;
+		} else if(c1[1] == c2[1]){
+			x = distance;
+		} else {
+			var slope = this.slope(c1,c2);
+			x = distance-slope
+			y = slope*x;
+		}
 		
 		//apply direction
 		if(c1[0] > c2[0]) x = -x;
 		if(c1[1] > c2[1]) y = -y;
 		
 		c3 =[c1[0]+x,c1[1]+y];
-		
 		if(constrain !== false){
 			//stop movement at desired location
 			if(x > 0){//moving right
@@ -412,11 +421,13 @@ _game.math = {
 				if(c3[1]<c2[1]) c3[1] = c2[1];
 			}
 		}
-		
+
 		return c3;
 	},
 	slope:function(c1,c2){
-		return (c1[1]-c2[1])/(c1[0]-c2[0]);
+		var out = (c1[1]-c2[1])/(c1[0]-c2[0]);
+		if(out == Infinity || isNaN(out)) out = 0;
+		return out;
 	}
 };
 
@@ -577,7 +588,7 @@ _game.draw = {
 
 			var translate = [renderState.position[0]-x,renderState.position[1]-y];
 			
-			console.log('translate: ',translate);
+			//console.log('translate: ',translate);
 			//console.log(renderState.position);
 			//console.log(serverData.position);
 			// apply movement translated from last rendered position to current
